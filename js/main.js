a0a99cf5d450f201db1f8f073b40c36dbacad129
JacsonPolonha <jacsonpolonha@gmail.com>
2024-01-30 21:44:56 -0300

    Adicionar a funcao copiar que copia o texto utilizando um botao

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+let result = document.querySelector('.result-message');
+
+function criptografar() {
+    let texto = document.querySelector('#texto').value;
+
+    result.innerHTML = texto.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
+    let botao = document.querySelector('.copia');
+    botao.style.display = 'block';
+
+}
+
+function descriptografar() {
+    let texto = document.querySelector('#texto').value;
+    result.innerHTML = texto.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');
+}
+
+function copiar() {
+    let result = document.querySelector('.result-message');
+    let resultText = result.innerText;
+    navigator.clipboard.writeText(resultText);
+}
